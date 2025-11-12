7a9e4b9fe78ee834c93b8d83befbbcd21ba9894b
ahlam201601 <elhadriahlam9@gmail.com>
2025-11-12 16:20:39 +0100

    affichage Questin

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import './App.css';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import Result from './pages/Result';
+
 import { BrowserRouter , Routes , Route  } from 'react-router';
 function App() {
   return (
     <BrowserRouter>
+
     <Routes>
       <Route  path='/' element={<Home/>}/>
       <Route  path='/Quiz' element={<Quiz/>}/>
       <Route  path='/Result' element={<Result/>}/>
-
     </Routes>
-      
-      
     </BrowserRouter>
   );
 }
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+import questions from '../data/questions'
 
 export default function Quiz() {
+
+  // const [question , setQuestion] = useState([])
+  const [options , setOptions] = useState([0])
+  const [index , setIndex] =useState(0)
+  const [ score , setScore ] = useState(0)
+  const handleScore = (option)=>{
+    if(option === questions[0].answer){
+      setScore(score + 1)
+    }
+    setIndex(index + 1)
+    
+  } 
+  console.log(score)
+
+ 
+
   return (
-    <div>Quiz</div>
+    <div>
+      <h1>{questions[index].question}</h1>
+      <button onClick={()=>handleScore(questions[0].options[0])}>{questions[0].options[0]}</button><br/><br/>
+      <button onClick={()=>handleScore(questions[0].options[1])}>{questions[0].options[1]}</button><br/><br/>
+      <button onClick={()=>handleScore(questions[0].options[2])}>{questions[0].options[2]}</button><br/><br/>
+      <button onClick={()=>handleScore(questions[0].options[1])}>{questions[0].options[3]}</button><br/><br/>
+    </div>
   )
 }
