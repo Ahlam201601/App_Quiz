009bae9de6d114fcdfd3e93dd4aae4bf6f18b68e
ahlam201601 <elhadriahlam9@gmail.com>
2025-11-12 22:17:52 +0100

    score

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -5,11 +5,13 @@ import questions from '../data/questions'
 export default function Quiz() {
 
   // const [question , setQuestion] = useState([])
-  const [options , setOptions] = useState([0])
-  const [index , setIndex] =useState(0)
+  // const [options , setOptions] = useState([0])
+  
   const [ score , setScore ] = useState(0)
+  const [index , setIndex] =useState(0)
+
   const handleScore = (option)=>{
-    if(option === questions[0].answer){
+    if(option === questions[index].answer){
       setScore(score + 1)
     }
     setIndex(index + 1)
@@ -17,15 +19,27 @@ export default function Quiz() {
   } 
   console.log(score)
 
- 
+
+//  const passer=()=>{
+//   setIndex(index+1)
+//  }
 
   return (
     <div>
       <h1>{questions[index].question}</h1>
-      <button onClick={()=>handleScore(questions[0].options[0])}>{questions[0].options[0]}</button><br/><br/>
-      <button onClick={()=>handleScore(questions[0].options[1])}>{questions[0].options[1]}</button><br/><br/>
-      <button onClick={()=>handleScore(questions[0].options[2])}>{questions[0].options[2]}</button><br/><br/>
-      <button onClick={()=>handleScore(questions[0].options[1])}>{questions[0].options[3]}</button><br/><br/>
+      {/* <button onClick={()=>handleScore(questions[index].options[0])}>{questions[index].options[0]}</button><br/><br/>
+      <button onClick={()=>handleScore(questions[index].options[1])}>{questions[index].options[1]}</button><br/><br/>
+      <button onClick={()=>handleScore(questions[index].options[2])}>{questions[index].options[2]}</button><br/><br/>
+      <button onClick={()=>handleScore(questions[index].options[3])}>{questions[index].options[3]}</button><br/><br/> */}
+
+      
+      {questions[index].options.map((option) => (
+        <div>
+          <button onClick={() => handleScore(option)}>{option}</button>
+          <br />
+          <br />
+        </div>
+       ))}
     </div>
   )
 }
